1b814e3ae1b66145080a97ab9278735fbedcb7d2
demondaze <darylsburrows@gmail.com>
2013-09-06 11:01:11 +0100

    added wesite files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="default/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="default/css/mystyles.css" rel="stylesheet" media="screen">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    
+    
+    <script src="default/js/bootstrap.js"></script> 
+    <script src="default/js/myscript.js"></script>
+  </body>
+</html>
